5c6339778799388e9e42971d2d517e24549b6bf0
Agustin Ferreira <agustin@xivis.com>
2018-09-08 13:42:34 +0200

    Created endpoints & serializers: POST requests, GET requests (all / by contract), POST hints

---
+++ off-chain/webproject/core/serializers.py
@@ -0,0 +1,30 @@
+from core.models import *
+from rest_framework import serializers
+
+
+class HintNotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HintNotification
+        fields = ("find_request", "notified", "index_in_contract")
+
+    def create(self, validated_data):
+        _hn = HintNotification()
+        for key in self.fields:
+            setattr(_hn, key, validated_data[key])
+
+        return _hn
+
+
+class RequestForFindSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = RequestForFind
+        fields = ("identifier", "first_name", "last_name", "photo", "description", "creator_address", "creator_email",
+                  "contract_deployed_address", "finished")
+
+    def create(self, validated_data):
+        _rff = RequestForFind()
+        for key in self.fields:
+            setattr(_rff, key, validated_data[key])
+
+        return _rff
