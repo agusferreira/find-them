59b59326befa44e3fe8f63a8b11d5833751f5645
Sol Rubado <sol@xivis.com>
2018-09-08 16:55:12 +0200

    Hints style

--- on-chain/src/containers/details/Details.js
+++ on-chain/src/containers/details/Details.js
@@ -10,18 +10,25 @@ class Details extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            hints: [{type:'success',hint:"Waldo was seen in Full Node programming"},
-                {type:'danger',hint:"Waldo was seen in Berghain drunk AF"},
-                {type:'info', hint:"Waldo was in ETHBerlin pitball"},
-                {type:'success',hint:"Waldo was seen in Full Node programming"},
-                {type:'danger',hint:"Waldo was seen in Berghain drunk AF"},
-                {type:'info', hint:"Waldo was in ETHBerlin pitball"}]
+            hints: [{type:'success',hint:"Waldo was seen in Full Node programming", isAdmin:false},
+                {type:'info', hint:"Waldo was in ETHBerlin pitball", isAdmin:true},
+                {type:'success',hint:"Waldo was seen in Full Node programming", isAdmin:false},
+                {type:'info', hint:"Waldo was in ETHBerlin pitball", isAdmin:true}]
         }
     }
 
+    _rejectHint = (id) =>{
+
+    };
+
+    _acceptHint = (id) =>{
+
+    };
+
     _renderHints = ()=>{
         return this.state.hints.map((hint,index)=>{
-            return <Tips key={hint.hint} type={hint.type} title={`Hint ${index+1}`} hint={hint.hint}/>})
+            return <Tips key={hint.hint} type={hint.type} title={`Hint ${index+1}`} hint={hint.hint}
+                         editable={hint.isAdmin} acceptAction={this._acceptHint} rejectAction={this._rejectHint}/>})
     };
 
     render() {
@@ -32,7 +39,6 @@ class Details extends Component {
                               date={"07/09/2018"} contact={"john.doe@gmail.com"}
                               description={"1.80m, dark hair, blue eyes"}
                               incentive={"12 ETH"}/>
-
                 <Grid>
                     <Row>
                         <Col xs={12}>
--- on-chain/src/containers/details/components/tips/Tips.js
+++ on-chain/src/containers/details/components/tips/Tips.js
@@ -1,22 +1,84 @@
 import React, {Component} from 'react';
 
-import {Panel,Grid} from 'react-bootstrap';
+import {Panel, Grid, Col, Row, Modal} from 'react-bootstrap';
+import MaterialIcon from 'material-icons-react';
 
 import './styles.scss';
+import Button from "../../../../components/button/Button";
 
 
 class Tips extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showModal: false,
+            action: '',
+            buttonsDisabled: false,
+        }
+    }
+
+    _showModal = (action) => {
+        this.setState({showModal: true, action});
+    };
+
+    _closeModal = () => {
+        this.setState({showModal: false});
+    };
+
     render() {
+        let {showModal, action, buttonsDisabled} = this.state;
+        let modalContent = action !== '' ? `Are you sure you want to ${action} this hint?` : '';
 
         return (
-            <Grid >
-                <Panel bsStyle={this.props.type}>
-                    <Panel.Heading>
-                        <Panel.Title componentClass="h3">{this.props.title}</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body>{this.props.hint}</Panel.Body>
+            <Grid>
+                <Panel className={`custom-panel ${this.props.type}`} bsStyle={this.props.type}>
+                    <Panel.Body>
+                        <Grid>
+                            <Row>
+                                <Col xs={10}>
+                                    <p style={{fontSize: 18, padding: 14}}>{this.props.hint}</p>
+                                </Col>
+                                {this.props.editable &&
+                                <Col xs={2}>
+                                    <Col xs={3} className={'pointer'} onClick={() => this._showModal("accept")}>
+                                        <MaterialIcon icon="check_circle_outline"
+                                                      style={{padding: 10}} size={'medium'} color="#32cd32"/>
+                                    </Col>
+                                    <Col xs={3} className={'pointer'} onClick={() => this._showModal("reject")}>
+                                        <MaterialIcon icon="highlight_off"
+                                                      style={{padding: 10}} size={'medium'} color="#FF4843"/>
+                                    </Col>
+                                </Col>}
+                            </Row>
+                        </Grid>
+                    </Panel.Body>
                 </Panel>
+
+                <Modal show={showModal} onHide={this._closeModal} className={'vertical-center'}>
+                    <Modal.Body>
+                        <Grid className={"row margin-top-20 margin-bottom-20 text-center"}>
+                            <Row>
+                                <Col xs={6}>
+                                    <p>{modalContent}</p>
+                                </Col>
+                            </Row>
+                            <Row className={"modal-buttons"}>
+                                <Col xs={6}>
+                                    <Button className={`blue ${buttonsDisabled ? 'disabled' : ''}`}
+                                            onClick={this._closeModal}>
+                                        Cancel
+                                    </Button>
+                                    <Button className={`blue-full ${buttonsDisabled ? 'disabled' : ''}`}
+                                            onClick={this.props.action}>
+                                        {action.toUpperCase()}
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Grid>
+                        {/*{this.state.ajaxInProgress ? <Spinner/> : <div><br/><br/></div>}*/}
+                    </Modal.Body>
+                </Modal>
 
             </Grid>
         )
