1f18bc30db2bdce8bf2c6e849938b820c5236809
nahu90 <naha90@gmail.com>
2018-09-09 03:59:27 +0200

    testing donation distribution sistem

--- off-chain/webproject/core/management/commands/check_contract_status.py
+++ off-chain/webproject/core/management/commands/check_contract_status.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from core.services import contract_service
+from core.models import RequestForFind
 
 
 class Command(BaseCommand):
@@ -7,5 +8,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('--- Process started ---')
-        contract_service.check_contract_status()
+        #contract_service.check_contract_status()
+        contract_service.send_near_addreses_to_factory(RequestForFind.objects.filter(finished=False).order_by('-id').first())
         print('--- Process finished ---')
